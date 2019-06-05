ab123e963c35cf135a24936fd6bce42e2ff25951
zhikiri <zhivitsa.kirill.gm@gmail.com>
2019-06-05 17:20:25 +0300

    refactor graph constructor and added remove vertex

--- Graph/graph.go
+++ Graph/graph.go
@@ -5,13 +5,24 @@ type Graph struct {
 	edges map[string]Edge
 }
 
-func NewGraph() *Graph {
-	return &Graph{
+func NewGraph(values ...int) *Graph {
+	graph := &Graph{
 		make(map[int]Vertex),
 		make(map[string]Edge),
 	}
+
+	if len(values) > 0 {
+		for _, value := range values {
+			graph.AddVertex(value)
+		}
+	}
+	return graph
 }
 
 func (g *Graph) AddVertex(value int) {
 	g.vertexes[value] = Vertex{value}
+}
+
+func (g *Graph) RemoveVertex(value int) {
+	delete(g.vertexes, value)
 }
--- Graph/graph_test.go
+++ Graph/graph_test.go
@@ -2,13 +2,28 @@ package graph
 
 import "testing"
 
-func TestNewGraph(t *testing.T) {
+func TestNewGraphWithoutValues(t *testing.T) {
 	graph := NewGraph()
 	if graph.vertexes == nil {
 		t.Error("`graph.vertexes` should not be nil")
 	}
 	if graph.edges == nil {
 		t.Error("`graph.edges` should not be nil")
+	}
+}
+
+func TestNewGraphWithValues(t *testing.T) {
+	values := []int{1, 2, 3, 4, 5}
+	graph := NewGraph(values...)
+
+	if len(graph.vertexes) != len(values) {
+		t.Errorf("`graph.vertexes` should be filled with %d values", len(values))
+	}
+
+	for _, value := range values {
+		if _, ok := graph.vertexes[value]; ok != true {
+			t.Errorf("Value %+v should be present in graph", value)
+		}
 	}
 }
 
@@ -26,4 +41,22 @@ func TestAddVertex(t *testing.T) {
 		test(value, i + 1)
 	}
 	test(1, 4)
+}
+
+func TestRemoveVertex(t *testing.T) {
+	tests := []int{3, 5, 1, 8, 4}
+	graph := NewGraph(tests...)
+
+	test := func (value, expected int) {
+		graph.RemoveVertex(value)
+		if actual := len(graph.vertexes); expected != actual {
+			t.Errorf("Expected %+v, actual %+v", expected, actual)
+		}
+	}
+
+	i := len(tests)
+	for _, value := range tests {
+		i--
+		test(value, i)
+	}
 }
