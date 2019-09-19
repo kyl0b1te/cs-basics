93e5ab21d6b597f2402783a6f4df12351de60fe2
zhikiri <zhivitsa.kirill.gm@gmail.com>
2019-09-19 12:18:34 +0300

    complement the linear search test

--- algorithms/LinearSearch/linear_search_test.go
+++ algorithms/LinearSearch/linear_search_test.go
@@ -15,7 +15,7 @@ func (n TestNode) compare(with interface{}) bool {
 	return n.value == node.value
 }
 
-func getTestData() []Comparable {
+func getSortedTestData() []Comparable {
 	tests := []Comparable{}
 	for i := 0; i <= 5; i++ {
 		var node Comparable = TestNode{i}
@@ -24,12 +24,26 @@ func getTestData() []Comparable {
 	return tests
 }
 
+func getUnsortedTestData() []Comparable {
+	tests := []Comparable{}
+	for _, value := range []int{34, 1, 64, 12, 97} {
+		var node Comparable = TestNode{value}
+		tests = append(tests, node)
+	}
+	return tests
+}
+
 func TestLinearSearch(t *testing.T) {
 
-	tests := getTestData()
+	tests := getSortedTestData()
 	for i, test := range tests {
 		assert.Equal(t, i, LinearSearch(tests, test))
 	}
+	assert.Equal(t, -1, LinearSearch(tests, TestNode{99}))
 
+	tests = getUnsortedTestData()
+	for i, test := range tests {
+		assert.Equal(t, i, LinearSearch(tests, test))
+	}
 	assert.Equal(t, -1, LinearSearch(tests, TestNode{99}))
 }
